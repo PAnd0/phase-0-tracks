ca38505b3edc5c7463f74038bb1bb23b5105f5a6
Patrick Anderson <Patrick.Anderson.000@gmail.com>
2016-09-16 20:36:52 -0700

    initial commit

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,9 @@
+# Create an encrypt method
+#     define alphabet variable
+#     create counter
+#     For each letter in input string
+#         -find index of following letter in alphabet (add 1)
+#         -replace current letter with following letter in alphabet using index
+
+# get input string
+
