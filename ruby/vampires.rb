70d44ce119a710a17dad7e6c203bb24d30a0988f
Patrick Anderson <Patrick.Anderson.000@gmail.com>
2016-09-18 15:56:08 -0700

    addvampire data acquisition to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,26 @@
+
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.toi
+
+puts "What year were you born?"
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
+
+puts name
+puts age
+puts birth_year
+puts garlic_bread
+puts health_insurance
+
+# if age == 2016 - birth_year && garlic_bread == yes
+# else
+# end
