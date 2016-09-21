d0ed50afd229399b17c1179b80c38b2690c64342
Patrick Anderson <Patrick.Anderson.000@gmail.com>
2016-09-20 19:58:05 -0700

    uncomment code iteration.mb

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,10 +1,10 @@
-# def block_method
-#   puts "before block"
-#   yield ("This is a string")
-#   puts "after block"
-# end
+def block_method
+  puts "before block"
+  yield ("This is a string")
+  puts "after block"
+end
 
-# block_method { |string| puts string }
+block_method { |string| puts string }
 
 
 colors = ["blue", "red", "purple", "green", "black", "brown"]
@@ -17,65 +17,65 @@ player_scores = {
   benjamin: 22.8
 }
 
-# p colors
+p colors
 
-# colors.each do |color|
-#   puts color
-# end
+colors.each do |color|
+  puts color
+end
 
-# p colors
+p colors
 
-# puts player_scores
+puts player_scores
 
-# player_scores.each do |player, score|
-#   puts "#{player} : #{score}"
-# end
+player_scores.each do |player, score|
+  puts "#{player} : #{score}"
+end
 
-# puts player_scores
+puts player_scores
 
-# p colors
+p colors
 
-# colors.map! do |color|
-#   puts color
-#   color + " is a color"
-# end
+colors.map! do |color|
+  puts color
+  color + " is a color"
+end
 
-# p colors
+p colors
 
-# puts player_scores
+puts player_scores
 
-# player_scores.map do |player, score|
+player_scores.map do |player, score|
 
-#   score += 1
-#   puts "#{player} : #{score}"
-# end
+  score += 1
+  puts "#{player} : #{score}"
+end
 
-# puts player_scores
+puts player_scores
 
-# p colors
-# colors.delete_if {|color| color[0] == 'b'}
-# p colors
+p colors
+colors.delete_if {|color| color[0] == 'b'}
+p colors
 
-# p player_scores
-# player_scores.delete_if {|player, score| player == :benjamin || score < 10}
-# p player_scores
+p player_scores
+player_scores.delete_if {|player, score| player == :benjamin || score < 10}
+p player_scores
 
 
-# p colors
-# colors.keep_if {|color| color[0] == 'b'}
-# p colors
+p colors
+colors.keep_if {|color| color[0] == 'b'}
+p colors
 
-# p player_scores
-# player_scores.keep_if {|player, score| player == :benjamin || score < 10}
-# p player_scores
+p player_scores
+player_scores.keep_if {|player, score| player == :benjamin || score < 10}
+p player_scores
 
-# p colors
-# new_colors = colors.select {|color| color[0] == 'b'}
-# p new_colors
+p colors
+new_colors = colors.select {|color| color[0] == 'b'}
+p new_colors
 
-# p player_scores
-# new_player_scores = player_scores.select {|player, score| player == :benjamin || score < 10}
-# p new_player_scores
+p player_scores
+new_player_scores = player_scores.select {|player, score| player == :benjamin || score < 10}
+p new_player_scores
 
 p colors
 new_colors = colors.drop_while {|color| color[0] == 'b'}
