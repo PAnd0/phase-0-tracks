d175018b90a0afaf8b7dbc708cc04d20c2ac9ca3
Patrick Anderson <Patrick.Anderson.000@gmail.com>
2016-09-25 20:32:25 -0700

    add deck of cards nested data structure and calls to nested_data_structures.rb

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,12 @@
+
+# Deck of Cards
+deck = {
+  hearts: ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king'],
+  diamonds: ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king'],
+  spades: ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king'],
+  clubs: ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']
+}
+
+p deck[:hearts][5]
+
+p deck[:spades]
