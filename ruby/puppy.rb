959a6a4afd24d4c26dd14a959ffabea31b2b0cfd
Patrick Anderson <Patrick.Anderson.000@gmail.com>
2016-09-29 22:31:03 -0700

    add puppy class design notes to puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES--------------------------
+# Puppus puppus
+
+# CHARACTERISTICS------------------
+# Leg count: 4
+# Friendly: yes
+# Eye count: 2
+# Name: Varies
+# Color: Varies
+
+# BEHAVIOR------------------------
+# Jump
+# Lick
+# Bark
