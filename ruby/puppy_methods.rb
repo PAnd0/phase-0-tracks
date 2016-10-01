1cccd342ca411f7e805009338ba8b37894824ca0
Patrick Anderson <Patrick.Anderson.000@gmail.com>
2016-10-01 16:49:24 -0700

    add dog_years and play_dead methods to puppy_methods.rb

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -13,6 +13,13 @@ class Puppy
     puts "*rolls over*"
   end
 
+  def dog_years(human_yrs)
+    human_yrs * 7
+  end
+
+  def play_dead
+    puts "I'm dead."
+  end
 end
 
 spot = Puppy.new
@@ -22,4 +29,9 @@ spot.fetch("ball")
 spot.speak(5)
 
 spot.roll_over
+
+p spot.dog_years(12)
+
+spot.play_dead
+
 
