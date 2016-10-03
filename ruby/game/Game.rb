f9e5d7e4c3c2ecfa00c8acb91d8a4b6204657897
Patrick Anderson <Patrick.Anderson.000@gmail.com>
2016-10-03 04:18:54 -0700

    finish driver code user interface and pseudocode

--- ruby/game/Game.rb
+++ ruby/game/Game.rb
@@ -1,5 +1,17 @@
 # game class design
-# - Create class Game
+# - create class Game
+#     - create initialize method
+#       - create hash with key = word and value = blanks
+#       - create guess count attribute and set to 3 * the number of letters in the word
+#     - attribute accessors for word hash and guess count
+#     - create method to decrement guess count
+#     - create method to return word value (blanks) from hash
+#     - create method to fill in blanks with guess letter wherever that letter is found in word.
+#     - create method to check whether a guess is a repeated guess
+#     - create method to check whether the word has been fully guessed correctly.
+#     - create rspec tests for each method.
+# - add driver code to get word from player 1 and loop, updating word until player 2 wins or loses.
+
 
 # game class
 class Game
@@ -47,6 +59,23 @@ puts "Welcome to Definitely Not Hangman!"
 puts "Player 1, please enter a word to be guessed by Player 2."
 game = Game.new(gets.chomp)
 
-puts "Player 2, please enter a guess!"
+while game.guess_count > 0
+  puts "Player 2, here is the word for you to guess: #{game.word_state}"
+  puts "Please enter a letter guess!"
+  guess = gets.chomp
 
-p game.word
+  if !game.repeat_guess?(guess)
+    game.update_word(guess)
+    game.use_guess
+  end
+
+  if game.word_guessed?
+    puts "The word is: #{game.word_state}"
+    puts "Congratulations! You've won the game!"
+    break
+  end
+end
+
+if !game.word_guessed?
+puts "Unfortunately you did not have your guessing hat on today. Please try harder next time."
+end
