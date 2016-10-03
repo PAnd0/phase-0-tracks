ec8625bb90a08f2a8999544d796cbf9f93d3c608
Patrick Anderson <Patrick.Anderson.000@gmail.com>
2016-10-02 18:45:07 -0700

    fix get_mad_at method and add driver code to test

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -21,11 +21,16 @@ class Santa
   end
 
   def get_mad_at(reindeer_name)
-    @reindeer_ranking.delete(reindeer_name).push(reindeer_name)
+    @reindeer_ranking.delete(reindeer_name)
+    @reindeer_ranking.push(reindeer_name)
   end
 
   def set_gender(gender)
     @gender = gender
+  end
+
+  def reindeer_ranking
+    @reindeer_ranking
   end
 
 
@@ -36,14 +41,17 @@ Jamie = Santa.new("female", "asian")
 
 Jamie.speak
 Jamie.eat_milk_and_cookies("snickerdoodle")
+p Jamie.reindeer_ranking
+Jamie.get_mad_at("Prancer")
+p Jamie.reindeer_ranking
 
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "unfamiliar with gender concept"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "Extraterrestrial"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "unfamiliar with gender concept"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "Extraterrestrial"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
 
-santas.each do |santa|
-  p santa
-end
+# santas.each do |santa|
+#   p santa
+# end
