12d6cb0730eccb3f5ebda4551d5d774b13e7a19b
Patrick Anderson <Patrick.Anderson.000@gmail.com>
2016-10-09 20:25:45 -0700

    add Car constructor and test code

--- js/data_structures.js
+++ js/data_structures.js
@@ -9,6 +9,26 @@ for ( var i = 0; i < horseNames.length; i++) {
   stable[horseNames[i]] = colors[i];
 }
 
-//test stable creation
-console.log(stable);
+function Car(color, year) {
+  this.color = color;
+  this.year = year;
+  this.honk = function() {console.log("beep beep!");}
+}
+
+//test car constructor
+var myCar = new Car("red", 2003);
+
+console.log(myCar.color);
+console.log(myCar.year);
+myCar.honk();
+
+var myOtherCar = new Car("gray", 2007);
+
+console.log(myOtherCar.color);
+console.log(myOtherCar.year);
+myOtherCar.honk();
+
+
+// //test stable creation
+// console.log(stable);
 
