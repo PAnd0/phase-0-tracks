ea921f8fc0ca7c99ac9cce2d2bbd1fd8d80b9ca4
Patrick Anderson <Patrick.Anderson.000@gmail.com>
2016-10-09 20:03:38 -0700

    add for loop to create stable object from horseNames and colors arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,6 +1,14 @@
-var colors = ["blue", "green", "red", "yellow"];
 var horseNames = ["Martin", "Spot", "Ed", "Sparkles"];
+var colors = ["blue", "green", "red", "yellow"];
 
 colors.push("purple");
 horseNames.push("Chuck");
+
+var stable = {}
+for ( var i = 0; i < horseNames.length; i++) {
+  stable[horseNames[i]] = colors[i];
+}
+
+//test stable creation
+console.log(stable);
 
