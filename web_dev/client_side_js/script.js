5e314039bd52da8ae15a12a03885398adfddd31c
Patrick Anderson <Patrick.Anderson.000@gmail.com>
2016-10-23 19:09:58 -0700

    add js to change pic with button presses:

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,22 @@
 console.log("The script is running!");
+
+// function addBlueBackground(event) {
+//   var photo = document.getElementById("guy-pic");
+//   photo.style.border = "1px solid blue";
+// }
+
+function swapPiratePic(event) {
+  var photo = document.getElementById("guy-pic");
+  photo.src="images/pirate.jpg";
+}
+
+function swapConcernedPic(event) {
+  var photo = document.getElementById("guy-pic");
+  photo.src="images/concerned.gif"
+}
+
+var pirateButton = document.getElementById("pirate-button");
+pirateButton.addEventListener("click", swapPiratePic);
+
+var concernedButton = document.getElementById("concerned-button");
+concernedButton.addEventListener("click", swapConcernedPic);
