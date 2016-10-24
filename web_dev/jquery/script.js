48607bc16d572413f9fbeac1757f82979b7e5499
Patrick Anderson <Patrick.Anderson.000@gmail.com>
2016-10-23 21:25:19 -0700

    add troll hover functionality

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,26 +1,33 @@
-// function swapPiratePic(event) {
-//   document.getElementById("guy-pic").src="images/pirate.jpg";
-//   document.getElementById("pirate-button").style.visibility='hidden';
-//   document.getElementById("concerned-button").style.visibility='visible';
-// }
-
-// function swapConcernedPic(event) {
-//   document.getElementById("guy-pic").src="images/concerned.gif"
-//   document.getElementById("concerned-button").style.visibility='hidden';
-//   document.getElementById("pirate-button").style.visibility='visible';
-// }
-
-// var pirateButton = document.getElementById("pirate-button");
-// pirateButton.addEventListener("click", swapPiratePic);
-
-// var concernedButton = document.getElementById("concerned-button");
-// concernedButton.addEventListener("click", swapConcernedPic);
-
 $(document).ready(function() {
-  $("#pirate").hide();
+  $('#pirate').hide();
+  $('#troll').hide();
 
-  $("#pirate-button").click(function() {
-    $("#concerned").hide();
-    $("#pirate").show();
+  $('#pirate-button').click(function() {
+    $('#concerned').hide();
+    $('#pirate').show();
   });
-})
+
+  $('#concerned-button').click(function() {
+    $('#pirate').hide();
+    $('#concerned').show();
+  });
+
+  $('.guy-pic').hover(
+    function() {
+      $(this).data('src', $(this).attr('src'));
+      $(this).attr('src', $(this).attr('data-hover'));
+    },
+    function() {
+      $(this).attr('src', $(this).data('src'));
+    });
+
+  // $('.guy-pic').hover(
+  //   var hoveredPic = $(this)
+  //   var origSrc = $(this).attr('src');
+  //   function() {
+  //     $(this).attr('src','images/troll.jpg');
+  //   },
+  //   function() {
+  //     $(this).attr('src',origSrc);
+  //   });
+});
